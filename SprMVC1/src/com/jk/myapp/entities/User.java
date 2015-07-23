b4f75ebb06fcb23bd7563253d3548e1963527ade
kjamandl <kjamandl@gmail.com>
2015-07-23 18:37:57 +0530

    Added User Entity

---
+++ SprMVC1/src/com/jk/myapp/entities/User.java
@@ -0,0 +1,40 @@
+package com.jk.myapp.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="JK_USERS")
+public class User {
+
+	@Id
+	@GeneratedValue
+	@Column(name="USER_ID")
+	private Long userId;
+	@Column(name="USER_NAME")
+	private String userName;
+	@Column(name="USER_EMAIL")
+	private String userEmail;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+}
